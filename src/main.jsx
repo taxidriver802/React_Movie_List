978a2bb6231f4c633f35b08eb4fb32a1d173b01e
jason cox <JAcox12@icloud.com>
2025-06-27 04:27:01 -0500

    fixed gh-pages 404 error

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/React_Movie_List">
       <App />
     </BrowserRouter>
   </StrictMode>
